114581131a8bb421f9fa307f43db72d9950799ca
SebastianButilier <Sb2022073897@virtual.upt.pe>
2024-05-04 17:29:16 -0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+//const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "GET Hello Elysia")
+  .post("/hello", () => "POST world")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
